941942d3098fe2d2f9de2d45882370cbf733a528
Daniel Williams <daniel.williams@talktotucker.com>
2020-02-29 17:27:06 -0500

    Add runTimer function

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import java.util.Locale;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runTimer();
     }
 
     public void onClickStart(View view) {
@@ -26,6 +30,27 @@ public class MainActivity extends AppCompatActivity {
     public void onClickReset(View view) {
         running = false;
         seconds = 0;
+    }
+
+    public void runTimer() {
+        final TextView timeView = findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable() {
+           @Override
+           public void run() {
+               int hours = seconds/3600;
+               int minutes = (seconds%3600)/60;
+               int secs = seconds%60;
+               String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+               timeView.setText(time);
+               if (running) {
+                   // This isn't 100% accurate as there will be a slight delay over time.
+                   seconds++;
+               }
+               handler.postDelayed(this, 1000);
+           }
+        });
     }
 
 }
